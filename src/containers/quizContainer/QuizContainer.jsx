e1530b90cce881f8de43cdf40563a0271c40d22c
Justin <justinchongzhiyu@gmail.com>
2023-03-08 17:42:08 +0800

    feat: quiz questions template

--- src/containers/quizContainer/QuizContainer.jsx
+++ src/containers/quizContainer/QuizContainer.jsx
@@ -7,6 +7,9 @@ import AssistChip from "/src/components/assistChip/AssistChip";
 export default function QuizContainer() {
   const [question1Value, setQuestion1Value] = useState(null);
   const [question2Value, setQuestion2Value] = useState(null);
+  const [question3Value, setQuestion3Value] = useState(null);
+  const [question4Value, setQuestion4Value] = useState(null);
+  const [question5Value, setQuestion5Value] = useState(null);
 
   function handleQuestion1(event) {
     setQuestion1Value(event.target.value);
@@ -14,6 +17,15 @@ export default function QuizContainer() {
 
   function handleQuestion2(event) {
     setQuestion2Value(event.target.value);
+  }
+  function handleQuestion3(event) {
+    setQuestion3Value(event.target.value);
+  }
+  function handleQuestion4(event) {
+    setQuestion4Value(event.target.value);
+  }
+  function handleQuestion5(event) {
+    setQuestion5Value(event.target.value);
   }
 
   function handleSubmitQuiz() {
@@ -93,6 +105,114 @@ export default function QuizContainer() {
                   value="3"
                   checked={question2Value === "3"}
                   onChange={handleQuestion2}
+                />
+                This is something about MOSFETs number 3
+              </label>
+            </div>
+
+            <div>
+              <div>3) Explain something about MOSFETs</div>
+              <label>
+                <input
+                  type="radio"
+                  name="question3"
+                  value="1"
+                  checked={question3Value === "1"}
+                  onChange={handleQuestion3}
+                />
+                This is something about MOSFETs number 1
+              </label>
+              <br />
+              <label>
+                <input
+                  type="radio"
+                  name="question3"
+                  value="2"
+                  checked={question3Value === "2"}
+                  onChange={handleQuestion3}
+                />
+                This is something about MOSFETs number 2
+              </label>
+              <br />
+              <label>
+                <input
+                  type="radio"
+                  name="question3"
+                  value="3"
+                  checked={question3Value === "3"}
+                  onChange={handleQuestion3}
+                />
+                This is something about MOSFETs number 3
+              </label>
+            </div>
+
+            <div>
+              <div>4) Explain something about MOSFETs</div>
+              <label>
+                <input
+                  type="radio"
+                  name="question4"
+                  value="1"
+                  checked={question4Value === "1"}
+                  onChange={handleQuestion4}
+                />
+                This is something about MOSFETs number 1
+              </label>
+              <br />
+              <label>
+                <input
+                  type="radio"
+                  name="question4"
+                  value="2"
+                  checked={question4Value === "2"}
+                  onChange={handleQuestion4}
+                />
+                This is something about MOSFETs number 2
+              </label>
+              <br />
+              <label>
+                <input
+                  type="radio"
+                  name="question4"
+                  value="3"
+                  checked={question4Value === "3"}
+                  onChange={handleQuestion4}
+                />
+                This is something about MOSFETs number 3
+              </label>
+            </div>
+
+            <div>
+              <div>5) Explain something about MOSFETs</div>
+              <label>
+                <input
+                  type="radio"
+                  name="question5"
+                  value="1"
+                  checked={question5Value === "1"}
+                  onChange={handleQuestion5}
+                />
+                This is something about MOSFETs number 1
+              </label>
+              <br />
+              <label>
+                <input
+                  type="radio"
+                  name="question5"
+                  value="2"
+                  checked={question5Value === "2"}
+                  onChange={handleQuestion5}
+                />
+                This is something about MOSFETs number 2
+              </label>
+              <br />
+              <label>
+                <input
+                  type="radio"
+                  name="question5"
+                  value="3"
+                  checked={question5Value === "3"}
+                  onChange={handleQuestion5}
                 />
                 This is something about MOSFETs number 3
               </label>
